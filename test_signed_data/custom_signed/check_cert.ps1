d264d836fec421075ad0753a0c1caa0711671d9c
eyepatchSamurAI <incompleteness1357@gmail.com>
2023-08-25 16:22:54 -0500

    Updated pipeline to use correct exe for testing

--- test_signed_data/custom_signed/check_cert.ps1
+++ test_signed_data/custom_signed/check_cert.ps1
@@ -2,5 +2,6 @@ $domainName = "TotallyFakeTestDomain.com"
 
 $thumbprint = Get-ChildItem -Path Cert:\LocalMachine\Root | Where-Object { $_.Subject -like "*$domainName*" } | Select-Object -ExpandProperty Thumbprint
 $cert = Get-ChildItem Cert:\LocalMachine\Root | Where-Object { $_.Thumbprint -eq $thumbprint }
+
 # Print the subject
 Write-Output $cert.Subject
--- test_signed_data/custom_signed/setting_up_cert_testing.ps1
+++ test_signed_data/custom_signed/setting_up_cert_testing.ps1
@@ -1,11 +1,11 @@
 # Must be in admin
 $domainName = "TotallyFakeTestDomain.com"
 $signTool = "C:\Program Files (x86)\Windows Kits\10\App Certification Kit\signtool.exe"
-$password = "password"
 $name = "customCert"
 $certificatePath = ".\$name.pfx"
-$secureStringPassword = ConvertTo-SecureString -String $password -Force -AsPlainText
-$customExePath = ".\target\release\signed_exe.exe"
+$customExePath = ".\target\release\custom_signed_exe.exe"
+$destinationPath = "..\signed_exes\custom_signed_exe.exe"
+
 
 cargo build --release
 
@@ -13,10 +13,14 @@ cargo build --release
 openssl ecparam -out "$name.key" -name prime256v1 -genkey
 openssl req -new -sha256 -key "$name.key" -out "$name.csr" --config ".\openssl.cnf" -batch
 openssl x509 -req -sha256 -days 1 -in "$name.csr" -signkey "$name.key" -out "$name.crt"
-openssl pkcs12 -export -out "$name.pfx" -inkey "$name.key" -in "$name.crt"
+openssl pkcs12 -export -out "$name.pfx" -inkey "$name.key" -in "$name.crt" -passout pass:
 
 # Import a PFX certificate into a certificate store on the local computer
-$cert = Import-PfxCertificate -FilePath $certificatePath -CertStoreLocation Cert:\LocalMachine\Root -Password $secureStringPassword
+$cert = Import-PfxCertificate -FilePath $certificatePath -CertStoreLocation Cert:\LocalMachine\Root
 
 # Sign exe
-&$signTool sign /fd SHA256 /td sha256 /tr http://timestamp.digicert.com /f .\$name.pfx /p $password $customExePath
+&$signTool sign /fd SHA256 /td sha256 /tr http://timestamp.digicert.com /f .\$name.pfx $customExePath
+
+# Move signed exe 
+Move-Item -Path $customExePath -Destination $destinationPath -Force
+
