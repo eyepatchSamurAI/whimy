59450ffe24ce67d563dd89f93c3bc17a19333b79
eyepatchSamurAI <incompleteness1357@gmail.com>
2023-08-25 22:25:52 -0500

    Updated Examples

--- examples/src/verify_signature.mjs
+++ examples/src/verify_signature.mjs
@@ -1,4 +1,4 @@
-import { verifySignatureByPublishName } from "whimy"
+import { verifySignatureByPublisherNames } from "whimy"
 import { fileURLToPath } from 'url';
 import {resolve, dirname } from "path"
 
@@ -6,6 +6,6 @@ import {resolve, dirname } from "path"
     const filename = fileURLToPath(import.meta.url);
     const directoryName = dirname(filename);
     const filePath = resolve(directoryName, '../../test_signed_data/signed_exes/microsoft_signed.exe');
-    const output = verifySignatureByPublishName(filePath, ['CN="Microsoft Corporation",O="Microsoft Corporation",L=Redmond,S=Washington,C=US"'])
+    const output = verifySignatureByPublisherNames(filePath, ['CN="Microsoft Corporation",O="Microsoft Corporation",L=Redmond,S=Washington,C=US"'])
     console.log(output); 
 })()
