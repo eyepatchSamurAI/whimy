360a5ab1821d0939634ef5e95d4117b80c541047
eyepatchSamurAI <incompleteness1357@gmail.com>
2023-09-02 09:25:13 -0500

    Updated wmi examples

--- examples/src/wmi.mjs
+++ examples/src/wmi.mjs
@@ -1,8 +1,15 @@
 import { Wmi } from "whimy"
 
+// Sync Query
 (()=> {
     const wmi = new Wmi(`root\\cimv2`);
-    const result = wmi.query("select Name, ProcessId from Win32_Process");
-    console.log("result: ", JSON.parse(result));
+    const result = wmi.syncQuery("select Name, ProcessId from Win32_Process");
+    console.log("result: ", result);
     wmi.stop();
+})()
+
+// Async Query
+(async ()=> {
+    const result = await Wmi.asyncQuery(`root\\cimv2`, "select Name, ProcessId from Win32_Process");
+    console.log("result: ", result);
 })()
